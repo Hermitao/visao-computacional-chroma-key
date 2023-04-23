e75fe79e763f6edc515a52d4e0582c32f32970ca
Hermitao <vkriegergranemann@gmail.com>
2023-04-22 21:09:16 -0300

    Adiciona Ricardo Milos e resultados finais

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+# ⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄
+# ⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄
+# ⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄
+# ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆
+# ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇
+# ⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇
+# ⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 max_green_distance = 30
 
-def alter_image(in_filename, out_filename, thresh = 100, background_path = "./Imagens/background.bmp"):
+def alter_image(in_filename, out_filename, thresh = 100, background_path = "./Imagens/background.bmp", prefix=""):
     background = cv2.imread(background_path)
     background_gray = cv2.imread(background_path, cv2.IMREAD_GRAYSCALE)
     image = cv2.imread(in_filename)
@@ -23,22 +39,22 @@ def alter_image(in_filename, out_filename, thresh = 100, background_path = "./Im
             # verifica se o pixel é mais verde do que qualquer outro canal 
             # por uma determinada distância, e ainda com valor acima de um threshold
             if (int(col[1]) - int(col[0]) > max_green_distance) and (int(col[1]) - int(col[2]) > max_green_distance) and col[1] > thresh:
-                img_c[i][j] = (0, 0, 0)
                 img_c[i][j] = background_resized[i][j]
             j+=1
         i+=1
 
-    out_filepath = f"./img-{out_filename}/{out_filename}.bmp"
-    cv2.imwrite(out_filepath, img_c)
+    out_filepath_no_extension = f"./img-{out_filename}/{prefix}{out_filename}"
+    out_filepath = f"{out_filepath_no_extension}.bmp"
 
+    cv2.imwrite(out_filepath, img_c)
 
     # Anti-aliasing ----------------------
 
-    img_edgy_color = cv2.imread(f"./img-{out_filename}/{out_filename}.bmp")
+    img_edgy_color = cv2.imread(f"{out_filepath_no_extension}.bmp")
     #img_rgb = cv2.cvtColor(img_edgy_color, cv2.COLOR_BGR2RGB)
     img_rgb = img_edgy_color.copy()
 
-    img_grayscale = cv2.imread(f"./img-{out_filename}/{out_filename}.bmp", cv2.IMREAD_GRAYSCALE)
+    img_grayscale = cv2.imread(f"{out_filepath_no_extension}.bmp", cv2.IMREAD_GRAYSCALE)
 
 
     g_kernel_v = np.array([[-1, 0, 1], 
@@ -62,13 +78,22 @@ def alter_image(in_filename, out_filename, thresh = 100, background_path = "./Im
     mask = im_bin_v + im_bin_h
     np.clip(mask, 0, 1, out=mask)
 
-    blur = cv2.GaussianBlur(255 - img_rgb, (25,25), 0)
+    blur1 = cv2.GaussianBlur(255 - img_rgb, (25,25), 0)
+    blur2 = cv2.GaussianBlur(255 - img_rgb, (75,75), 0)
+    blur3 = cv2.GaussianBlur(255 - img_rgb, (101,101), 0)
+
+    cv2.imwrite(f"{out_filepath_no_extension}_blur1.bmp", 255 - blur1)
+    cv2.imwrite(f"{out_filepath_no_extension}_blur2.bmp", 255 - blur2)
+    cv2.imwrite(f"{out_filepath_no_extension}_blur3.bmp", 255 - blur3)
 
     mask = mask.reshape((mask.shape[0], mask.shape[1], 1))
-    img_aa = np.where(mask!=0,blur,(img_rgb / 255).astype(dtype='float32'))
+    img_aa1 = np.where(mask!=0,blur1,(img_rgb / 255).astype(dtype='float32'))
+    img_aa2 = np.where(mask!=0,blur2,(img_rgb / 255).astype(dtype='float32'))
+    img_aa3 = np.where(mask!=0,blur3,(img_rgb / 255).astype(dtype='float32'))
 
-    #cv2.imwrite(f"./img-{out_filename}/{out_filename}_final.bmp", (img_aa * 255).astype(dtype='uint8'))
-    cv2.imwrite(f"./img-{out_filename}/{out_filename}_final.bmp", (img_aa * 255).astype(dtype='uint8'))
+    cv2.imwrite(f"{out_filepath_no_extension}_final.bmp", (img_aa1 * 255).astype(dtype='uint8'))
+    cv2.imwrite(f"{out_filepath_no_extension}_final.bmp", (img_aa2 * 255).astype(dtype='uint8'))
+    cv2.imwrite(f"{out_filepath_no_extension}_final.bmp", (img_aa3 * 255).astype(dtype='uint8'))
 
 
 alter_image("./Imagens/corvo.bmp", "corvo", 210)
@@ -76,7 +101,7 @@ alter_image("./Imagens/corvos.bmp", "corvos")
 alter_image("./Imagens/formas.bmp", "formas")
 alter_image("./Imagens/rainha.bmp", "rainha", 87)
 
-alter_image("./Imagens/corvo.bmp", "corvo", 210, background_path="./Imagens/background-bonus-3.bmp")
-alter_image("./Imagens/corvos.bmp", "corvos", background_path="./Imagens/background-bonus-3.bmp")
-alter_image("./Imagens/formas.bmp", "formas", background_path="./Imagens/background-bonus-3.bmp")
-alter_image("./Imagens/rainha.bmp", "rainha", 87, background_path="./Imagens/background-bonus-3.bmp")
+alter_image("./Imagens/corvo.bmp", "corvo", 210, background_path="./Imagens/background-bonus.bmp", prefix="bonus_")
+alter_image("./Imagens/corvos.bmp", "corvos", background_path="./Imagens/background-bonus-4.bmp", prefix="bonus_")
+alter_image("./Imagens/formas.bmp", "formas", background_path="./Imagens/background-bonus-2.bmp", prefix="bonus_")
+alter_image("./Imagens/rainha.bmp", "rainha", 87, background_path="./Imagens/background-bonus-3.bmp", prefix="bonus_")
